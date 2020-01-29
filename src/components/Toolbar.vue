4efa9b4b70bb67f4797061f39d011ecc90b716dc
Timur <timich1995@gmail.com>
2020-01-29 13:27:13 +0800

    FIX: Minor design changes.

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,11 +1,16 @@
 <template>
-    <v-toolbar app fixed clipped-left>
+    <v-toolbar dense app fixed clipped-left>
         <v-btn :to="{ name: 'HomePage' }" fab small flat color="white">
             <img style="width: 100%;" src="/images/chess-club-logo.svg" alt="John">
         </v-btn>
-        <v-btn :to="{ name: 'Competitions' }">Competitions</v-btn>
-        <v-btn :to="{ name: 'Players' }">Players</v-btn>
-        <v-toolbar-title></v-toolbar-title>
+        <v-btn flat :to="{ name: 'Competitions' }">
+            <v-icon left dark>assessment</v-icon>
+            Competitions
+        </v-btn>
+        <v-btn flat :to="{ name: 'Players' }">
+            <v-icon left dark>people</v-icon>
+            Players
+        </v-btn>
         <v-spacer></v-spacer>
         <!-- User menu -->
         <UserMenu />
