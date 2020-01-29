797dc75933c77fba37de252b47c140c4bce9ab7f
Timur <timich1995@gmail.com>
2020-01-29 16:09:29 +0800

    FIX: Toolbar buttons are now responsive.

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -3,14 +3,24 @@
         <v-btn :to="{ name: 'HomePage' }" fab small flat color="white">
             <img style="width: 100%;" src="/images/chess-club-logo.svg" alt="John">
         </v-btn>
-        <v-btn flat :to="{ name: 'Competitions' }">
+
+        <v-btn flat :to="{ name: 'Competitions' }" class="hidden-xs-only">
             <v-icon left dark>assessment</v-icon>
             Competitions
         </v-btn>
-        <v-btn flat :to="{ name: 'Players' }">
+        <v-btn flat :to="{ name: 'Players' }" class="hidden-xs-only">
             <v-icon left dark>people</v-icon>
             Players
         </v-btn>
+
+        <!-- ... -->
+        <v-btn fab small flat :to="{ name: 'Competitions' }" class="hidden-sm-and-up">
+            <v-icon>assessment</v-icon>
+        </v-btn>
+        <v-btn fab small flat :to="{ name: 'Players' }" class="hidden-sm-and-up">
+            <v-icon>people</v-icon>
+        </v-btn>
+        
         <v-spacer></v-spacer>
         <!-- User menu -->
         <UserMenu />
